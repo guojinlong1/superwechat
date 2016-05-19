5ea22a226664c4410de8e1afc3ded270472cd93f
guojinlong1 <869153318@qq.com>
2016-05-19 11:16:59 +0800

    将DemoApplication改成SuperWeChatApplication

--- src/cn/ucai/superwechat/activity/AddContactActivity.java
+++ src/cn/ucai/superwechat/activity/AddContactActivity.java
@@ -92,7 +92,7 @@ public class AddContactActivity extends BaseActivity{
 	 * @param view
 	 */
 	public void addContact(View view){
-		if(DemoApplication.getInstance().getUserName().equals(nameText.getText().toString())){
+		if(SuperWeChatApplication.getInstance().getUserName().equals(nameText.getText().toString())){
 			String str = getString(R.string.not_add_myself);
 			startActivity(new Intent(this, AlertDialog.class).putExtra("msg", str));
 			return;
