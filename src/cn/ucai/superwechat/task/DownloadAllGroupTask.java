c1dcec903437c48cf8c92a89ce2377e2a84cbed3
guojinlong1 <869153318@qq.com>
2016-05-23 10:47:56 +0800

    修改DownLoadAllGroupTask

--- src/cn/ucai/superwechat/task/DownloadAllGroupTask.java
+++ src/cn/ucai/superwechat/task/DownloadAllGroupTask.java
@@ -12,6 +12,7 @@ import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.SuperWeChatApplication;
 import cn.ucai.superwechat.activity.BaseActivity;
 import cn.ucai.superwechat.bean.Contact;
+import cn.ucai.superwechat.bean.Group;
 import cn.ucai.superwechat.data.ApiParams;
 import cn.ucai.superwechat.data.GsonRequest;
 import cn.ucai.superwechat.utils.Utils;
@@ -42,25 +43,20 @@ public class DownloadAllGroupTask extends BaseActivity {
     }
 
     public void execute(){
-        executeRequest(new GsonRequest<Contact[]>(path,Contact[].class
-                ,responseDownloadContactListTaskListener(),errorListener()));
+        executeRequest(new GsonRequest<Group[]>(path,Group[].class
+                ,responseDownloadAllGroupTaskListener(),errorListener()));
     }
 
-    private Response.Listener<Contact[]> responseDownloadContactListTaskListener() {
-        return new Response.Listener<Contact[]>() {
+    private Response.Listener<Group[]> responseDownloadAllGroupTaskListener() {
+        return new Response.Listener<Group[]>() {
             @Override
-            public void onResponse(Contact[] contacts) {
+            public void onResponse(Group[] contacts) {
                 if(contacts!=null){
-                    ArrayList<Contact> contactList=
-                            SuperWeChatApplication.getInstance().getContactList();
-                    ArrayList<Contact> list = Utils.array2List(contacts);
+                    ArrayList<Group> contactList=
+                            SuperWeChatApplication.getInstance().getGroupList();
+                    ArrayList<Group> list = Utils.array2List(contacts);
                     contactList.clear();
                     contactList.addAll(list);
-                    HashMap<String ,Contact> userList = SuperWeChatApplication.getInstance().getUserList();
-                    userList.clear();
-                    for (Contact c :list){
-                        userList.put(c.getMContactCname(),c);
-                    }
                     mContext.sendStickyBroadcast(new Intent("update_group_list"));
                 }
             }
