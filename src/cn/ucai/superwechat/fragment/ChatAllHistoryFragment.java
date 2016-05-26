f6126b3f8249d571e61fb8b40ded1b00ff7f576d
guojinlong1 <869153318@qq.com>
2016-05-27 00:23:56 +0800

    修正会话界面登录不显示头像问题和申请与通知显示提示 添加广播接收器

--- src/cn/ucai/superwechat/fragment/ChatAllHistoryFragment.java
+++ src/cn/ucai/superwechat/fragment/ChatAllHistoryFragment.java
@@ -6,8 +6,10 @@ import java.util.Comparator;
 import java.util.Hashtable;
 import java.util.List;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -69,6 +71,7 @@ public class ChatAllHistoryFragment extends Fragment implements OnClickListener 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
+		registerContactReceiver();
 		if(savedInstanceState != null && savedInstanceState.getBoolean("isConflict", false))
             return;
 		inputMethodManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -297,4 +300,25 @@ public class ChatAllHistoryFragment extends Fragment implements OnClickListener 
     @Override
     public void onClick(View v) {        
     }
+
+	class ContactListChangeReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			adapter.notifyDataSetChanged();
+		}
+	}
+	private ContactListChangeReceiver mContactListChangeReceiver;
+	private void registerContactReceiver () {
+		mContactListChangeReceiver = new ContactListChangeReceiver();
+		IntentFilter filter = new IntentFilter("update_contact_list");
+		getActivity().registerReceiver(mContactListChangeReceiver, filter);
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		if(mContactListChangeReceiver!=null){
+			getActivity().unregisterReceiver(mContactListChangeReceiver);
+		}
+	}
 }
